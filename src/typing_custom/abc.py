6e74797c47c6b74267a037721ef5e24b8c3d3d9d
Jacob Ogden <jacob.ogden@yahoo.com>
2025-10-23 13:02:09 -0400

    Parity with main

--- src/typing_custom/abc.py
+++ src/typing_custom/abc.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
   configure_logging()
 
 from logging import getLogger
-from threading import Lock
+
+from aiologic import Lock
 
 logger = getLogger(__name__)
 
@@ -20,25 +21,7 @@ class SingletonType(type):
   def __call__(self, *args, **kwargs):
     with self.__shared_instance_lock__:
       try:
-        instance = self.__shared_instance__
-        # Call __init__ on existing instance to allow updates (e.g., pbar)
-        instance.__init__(*args, **kwargs)
-        return instance
+        return self.__shared_instance__
       except AttributeError:
         self.__shared_instance__ = super(SingletonType, self).__call__(*args, **kwargs)
         return self.__shared_instance__
-
-
-class SingletonNamedInstances(type):
-  __shared_instances__: dict[str, "SingletonNamedInstances"]
-  __shared_instance_locks__: dict[str, Lock]
-
-  def __new__(mcs, name, bases, attrs):
-    cls = super(SingletonNamedInstances, mcs).__new__(mcs, name, bases, attrs)
-    cls.__shared_instances__ = {}
-    cls.__shared_instance_locks__ = {}
-    return cls
-
-  def __call__(self, name: str, *args, **kwargs):
-    with self.__shared_instance_locks__.setdefault(name, Lock()):
-      return self.__shared_instances__.setdefault(name, super(SingletonNamedInstances, self).__call__(*args, **kwargs))
