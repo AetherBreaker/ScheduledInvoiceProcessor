294e9f44ac2f236dd870ef775352d693e3576c2d
Jacob Ogden <jacob.ogden@yahoo.com>
2025-10-23 12:32:54 -0400

    Removed a no longer needed singleton class implementation

--- src/typing_custom/abc.py
+++ src/typing_custom/abc.py
@@ -25,18 +25,3 @@ class SingletonType(type):
       except AttributeError:
         self.__shared_instance__ = super(SingletonType, self).__call__(*args, **kwargs)
         return self.__shared_instance__
-
-
-class SingletonNamedInstances(type):
-  __shared_instances__: dict[str, "SingletonNamedInstances"]
-  __shared_instance_locks__: dict[str, Lock]
-
-  def __new__(mcs, name, bases, attrs):
-    cls = super(SingletonNamedInstances, mcs).__new__(mcs, name, bases, attrs)
-    cls.__shared_instances__ = {}
-    cls.__shared_instance_locks__ = {}
-    return cls
-
-  def __call__(self, name: str, *args, **kwargs):
-    with self.__shared_instance_locks__.setdefault(name, Lock()):
-      return self.__shared_instances__.setdefault(name, super(SingletonNamedInstances, self).__call__(*args, **kwargs))
