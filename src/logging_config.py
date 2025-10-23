db902aec1cb772445d9bcf75e1f620ef2bac14cc
Jacob Ogden <jacob.ogden@yahoo.com>
2025-10-23 12:28:16 -0400

    Fixed an issue with debug log events not arriving at the appropriate handlers

--- src/logging_config.py
+++ src/logging_config.py
@@ -162,7 +162,7 @@ def configure_logging():
 
   root = logging.getLogger()
   # root.setLevel(logging.DEBUG if __debug__ else logging.INFO)
-  root.setLevel(logging.INFO)
+  root.setLevel(logging.DEBUG)
 
   debugging_file_handler = daily_debug_handler if LOGGING_TYPE == "daily" else per_run_debug_handler
   debugging_file_handler.setLevel(logging.DEBUG)
@@ -181,6 +181,8 @@ def configure_logging():
     rich_tracebacks=True,
     log_time_format=logging_timestamp_fmt,
   )
+
+  console_info_handler.setLevel(logging.INFO)
 
   formatter = FixedFormatter(
     fmt=f"[{{asctime}}] {{levelname: >8}} | {{libpath: <{max_width}}} | {{message}}",
