9d6ebbd4234558662108f83542ed9b56ea60ff1a
Jacob Ogden <jacob.ogden@yahoo.com>
2026-03-20 13:36:00 -0400

    a

--- src/environment_settings.py
+++ src/environment_settings.py
@@ -45,13 +45,10 @@ class Settings(BaseSettings):
   sas_ftp_creds_file: Annotated[Path, Field(alias="SAS_FTP_CREDS_FILE")]
   ryo_ftp_creds_file: Annotated[Path, Field(alias="RYO_FTP_CREDS_FILE")]
 
-<<<<<<< HEAD
-=======
   file_serve_public_domain: Annotated[str, Field(alias="FILE_SERVE_PUBLIC_DOMAIN")] = "som.sweetfiretobacco.com"
   file_serve_host: Annotated[str, Field(alias="FILE_SERVE_HOST")] = "localhost"
   file_serve_port: Annotated[int, Field(alias="FILE_SERVE_PORT")] = 8080
 
->>>>>>> 24ae9bfa70f04b50faa84a43b25b7c3f67661c6b
   # @field_validator("*", mode="wrap", check_fields=False)
   # @classmethod
   # def log_failed_field_validations(cls, data: str, handler: ValidatorFunctionWrapHandler, info: ValidationInfo) -> Any:
