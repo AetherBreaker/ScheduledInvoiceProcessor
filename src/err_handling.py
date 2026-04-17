56ed4f82576fb9db5d93a0bdd42eb4e556e7ed28
Jacob Ogden <jacob.ogden@yahoo.com>
2026-04-17 14:14:33 -0400

    Modified err_handling to only handle uncaught exceptions while not debugging.

--- src/err_handling.py
+++ src/err_handling.py
@@ -80,34 +80,40 @@ def get_last_fatal_details() -> FatalDetails:
 
 #   return wrapper
 
+if not __debug__:
 
-def handle_fatal_exc[**TP, TR](func: Callable[TP, TR]) -> Callable[TP, TR | None]:
-  @wraps(func)
-  def wrapper(*args: TP.args, **kwargs: TP.kwargs) -> TR | None:
-    try:
-      return func(*args, **kwargs)
-    except CancelledError:
-      pass
-      raise  # raise whatever to make the type checker happy about return values
-    except BaseException as e:
-      if isinstance(e, CancelledError):
-        raise
-      _last_fatal_details["is_database_origin"] = _is_database_origin_exception(e)
-      _last_fatal_details["exception_type"] = type(e).__name__
-      _last_fatal_details["exception_message"] = str(e)
-      logger.critical(f"Fatal exception in {func.__qualname__}: {e}", exc_info=True)
-      # _exit(1)  # Exit with non-zero code to indicate failure to Coolify
+  def handle_fatal_exc[**TP, TR](func: Callable[TP, TR]) -> Callable[TP, TR | None]:  # type: ignore
+    @wraps(func)
+    def wrapper(*args: TP.args, **kwargs: TP.kwargs) -> TR | None:
+      try:
+        return func(*args, **kwargs)
+      except CancelledError:
+        pass
+        raise  # raise whatever to make the type checker happy about return values
+      except BaseException as e:
+        if isinstance(e, CancelledError):
+          raise
+        _last_fatal_details["is_database_origin"] = _is_database_origin_exception(e)
+        _last_fatal_details["exception_type"] = type(e).__name__
+        _last_fatal_details["exception_message"] = str(e)
+        logger.critical(f"Fatal exception in {func.__qualname__}: {e}", exc_info=True)
+        # _exit(1)  # Exit with non-zero code to indicate failure to Coolify
 
-      strio = StringIO()
+        strio = StringIO()
 
-      tmp = Console(force_terminal=False, force_interactive=False, color_system=None, width=100, markup=False, file=strio)
+        tmp = Console(force_terminal=False, force_interactive=False, color_system=None, width=100, markup=False, file=strio)
 
-      with tmp.capture() as capture:
-        tmp.print_exception(show_locals=True)
-      content = capture.get()
+        with tmp.capture() as capture:
+          tmp.print_exception(show_locals=True)
+        content = capture.get()
 
-      send_alert_email(f"Fatal exception in {func.__qualname__}", f"{e}:\n\n{content}")
-      FATAL_EVENT.set()
-      return None
+        send_alert_email(f"Fatal exception in {func.__qualname__}", f"{e}:\n\n{content}")
+        FATAL_EVENT.set()
+        return None
 
-  return wrapper
+    return wrapper
+
+else:
+
+  def handle_fatal_exc[**TP, TR](func: Callable[TP, TR]) -> Callable[TP, TR]:
+    return func
