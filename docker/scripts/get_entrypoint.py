3fb6535bc28e1edb6091d575cb69b8c8f4882eb1
Jacob Ogden <jacob.ogden@yahoo.com>
2026-07-17 11:49:38 -0400

    Bump version to 2.0.10

--- docker/scripts/get_entrypoint.py
+++
@@ -1,8 +0,0 @@
-# Standard library imports
-import tomllib
-
-with open("/app/pyproject.toml", "rb") as f:
-  data = tomllib.load(f)
-
-project_name = data["project"]["name"].replace("_", "-")
-print(project_name, end="")
